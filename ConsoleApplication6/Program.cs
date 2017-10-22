bc207fc3507cdb97cddec53734fec71b77da4fb0
LocalUser <LocalUser@K3-512-17>
2017-10-22 15:09:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CowCounter(45));
+        }
+
+        public static string CowCounter(int n)
+        {
+            int lastDigit = n % 10;
+            int last2Digits = n % 100;
+            int lastlast = last2Digits % 10;
+            string result = string.Empty;
+
+            if (11 <= last2Digits && last2Digits <= 19)
+                result = "Коров";
+            else if (last2Digits == 01)
+                result = "Корова";
+            else if (lastDigit == 1 && last2Digits != 11)
+                result = "Корова";
+            else if (2 <= lastDigit && lastDigit <= 4)
+                result = "Коровы";
+            else if (5 <= lastDigit && lastDigit <= 9 || lastDigit == 0 || last2Digits == 11 )
+                result = "Коров";
+            
+            
+            return string.Format("{0} {1}", n, result);
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication6;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n = 1;
+            string actual = Program.CowCounter(n);
+            string expected = "1 Корова";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            int n = 11;
+            string actual = Program.CowCounter(n);
+            string expected = "11 Коров";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod111()
+        {
+            int n = 111;
+            string actual = Program.CowCounter(n);
+            string expected = "111 Коров";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod121()
+        {
+            int n = 121;
+            string actual = Program.CowCounter(n);
+            string expected = "121 Корова";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod125()
+        {
+            int n = 125;
+            string actual = Program.CowCounter(n);
+            string expected = "125 Коров";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod489()
+        {
+            int n = 489;
+            string actual = Program.CowCounter(n);
+            string expected = "489 Коров";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod101()
+        {
+            int n = 101;
+            string actual = Program.CowCounter(n);
+            string expected = "101 Корова";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod1001()
+        {
+            int n = 1001;
+            string actual = Program.CowCounter(n);
+            string expected = "1001 Корова";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int n = 2;
+            string actual = Program.CowCounter(n);
+            string expected = "2 Коровы";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int n = 8;
+            string actual = Program.CowCounter(n);
+            string expected = "8 Коров";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod0()
+        {
+            int n = 0;
+            string actual = Program.CowCounter(n);
+            string expected = "0 Коров";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int n = 4;
+            string actual = Program.CowCounter(n);
+            string expected = "4 Коровы";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int n = 10;
+            string actual = Program.CowCounter(n);
+            string expected = "10 Коров";
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
